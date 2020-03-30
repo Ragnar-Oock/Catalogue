9453e65a424ff8bf80fe4333427cad142d15a875
ugo <ugo.dumontot@sinao.fr>
2020-03-30 11:53:23 +0200

    add name and address field in editor entity

--- src/Entity/Editor.php
+++ src/Entity/Editor.php
@@ -16,8 +16,42 @@ class Editor
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $address;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
     }
 }
