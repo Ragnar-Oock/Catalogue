80c8539485e9a74245bae7474c11f7beafce035a
ugo <ugo.dumontot@sinao.fr>
2020-04-26 15:52:27 +0200

    added default redirect to home page on login

--- src/Security/ConnexionAuthenticator.php
+++ src/Security/ConnexionAuthenticator.php
@@ -95,6 +95,7 @@ class ConnexionAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('site'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
