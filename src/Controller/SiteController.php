29a99e6cd4a6fae80c1d15baad4fde1d49692559
ugo <ugo.dumontot@sinao.fr>
2020-04-26 15:55:15 +0200

    added explore edition and explore author pages

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
+use App\Entity\Edition;
 use App\Form\SearchFormType;
+use App\Repository\AuthorRepository;
 use App\Repository\EditionRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -52,5 +55,54 @@ class SiteController extends AbstractController
         return $this->render('site/search/results.html.twig', [
             'resultList' => $res
         ]);
+    }
+
+
+    /**
+     * @Route("/explorer/auteur/{author}", name="explore_author")
+     */
+    public function showAuthor(Request $request, PaginatorInterface $paginator, Author $author)
+    {
+        if ($author != null) {
+            $bibliography = $author->getEditions();
+            $bibliography = $paginator->paginate(
+                $bibliography,
+                $request->query->get('page', 1),
+                15
+            );
+
+            return $this->render('site/explore/author.html.twig', [
+                'bibliography' => $bibliography,
+                'author' => $author
+            ]);
+        }
+        else {
+            dd($request->query->get('authorId'));
+        }
+    }
+    
+    /**
+     * @Route("/explorer/edition/{edition}", name="explore_edition")
+     */
+    public function showEdition(Request $request, PaginatorInterface $paginator, Edition $edition)
+    {
+
+        if ($edition != null) {
+            
+            $authors = $edition->getAuthors();
+            $authors = $paginator->paginate(
+                $authors,
+                $request->query->get('page', 1),
+                15
+            );
+
+            return $this->render('site/explore/edition.html.twig', [
+                'authors' => $authors,
+                'edition' => $edition
+            ]);
+        }
+        else {
+            dd($request->query->get('authorId'));
+        }
     }
 }
