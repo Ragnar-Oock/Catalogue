0194752977f1eb1e9d12d324b68f103fed2073d4
ugo <ugo.dumontot@sinao.fr>
2020-06-14 14:02:49 +0200

    re populate the advenced search form

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -15,9 +15,30 @@ class SearchController extends AbstractController
      * @Route("/recherche", name="search")
      */
     public function index(EditionRepository $editionRepository, PaginatorInterface $paginator, Request $request)
-    {        
-        $form = $this->createForm(AdvencedSearchFormType::class);
-        $results = $editionRepository->searchEdition($request->query->get('search', ''));
+    {
+        $values = $request->get('advenced_search_form');
+        $values = [
+            'publisheAfter' => !empty($values['publisheAfter']) ? \DateTime::createFromFormat('d/m/Y', $values['publisheAfter']) : null,
+            'publishedBefore' => !empty($values['publishedBefore']) ? \DateTime::createFromFormat('d/m/Y', $values['publishedBefore']) : null,
+            'type' => $values['type'],
+            'title' => $values['title'],
+            'author' => $values['author'],
+            'editor' => $values['editor'],
+            'issn' => $values['issn'],
+            'isbn' => $values['isbn'],
+        ];
+        $form = $this->createForm(AdvencedSearchFormType::class, $values, [
+            'action' => $this->generateUrl('search'),
+            'method' => 'GET',
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $results = $editionRepository->advencedSearchEdition($values);
+        }
+        else {
+            $results = $editionRepository->searchEdition($request->query->get('search', ''));
+        }
         $results = $paginator->paginate(
             $results,
             $request->query->get('page', 1),
