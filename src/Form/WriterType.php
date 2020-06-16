93aea7db5d128a4f19c55a098e752b42b44313d6
ugo <ugo.dumontot@sinao.fr>
2020-06-16 13:52:10 +0200

    update the writer crud

--- src/Form/WriterType.php
+++ src/Form/WriterType.php
@@ -2,10 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Author;
+use App\Entity\ParticipationType;
 use App\Entity\Writer;
+use App\Repository\AuthorRepository;
+use App\Repository\ParticipationTypeRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class WriterType extends AbstractType
@@ -13,10 +18,26 @@ class WriterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('participationType')
-            ->add('author')
-            ->add('save', SubmitType::class, ['label' => 'Enregistrer et retourner a l\'edition'])
-            ->add('saveAndAddMore', SubmitType::class, ['label' => 'Enregister et ajouter d\'autres participants'])
+            ->add('participationType', EntityType::class, [
+                'label' => 'Type de participation',
+                'class' => ParticipationType::class,
+                'query_builder' => function (ParticipationTypeRepository $pr) {
+                    return $pr->createQueryBuilder('p')
+                        ->orderBy('p.title', 'ASC');
+                },
+                'required'=> true
+            ])
+            ->add('author', EntityType::class, [
+                'label' => 'Auteur',
+                'class' => Author::class,
+                'query_builder' => function (AuthorRepository $ar) {
+                    return $ar->createQueryBuilder('a')
+                        ->orderBy('a.name', 'ASC');
+                },
+                'required'=> true
+            ])
+            // ->add('save', SubmitType::class, ['label' => 'Enregistrer et retourner a l\'edition'])
+            // ->add('saveAndAddMore', SubmitType::class, ['label' => 'Enregister et ajouter d\'autres participants'])
         ;
     }
 
